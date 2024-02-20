07ca2cc4904e283e248563f02b46ef1c426a9a87
julianlarra <larragueta.julian@gmail.com>
2024-02-20 11:47:22 -0300

    ejercicio menu de opciones

--- Clase3.py
+++ Clase3.py
@@ -1,12 +1,19 @@
 def mostrar_menu():
-        print("Opciones:")
+        print('#'*20  ) 
+        print(  "\n Opciones:")
         print("1. Realizar acción A (Suma)")
         print("2. Realizar acción B (Producto)")
-        print("0. Salir")
+        print("0. Salir \n"  )
+        print('/-'*10  )
 
+def opcion_a_suma(num1,num2):
+    suma = num1 + num2
+    return suma
 
-def opcion_a_suma():
-    pass
+def opcion_b_producto(num1,num2):
+     producto= num1 * num2
+     return producto
+    
 
 def main():
     opcion= 3
@@ -14,20 +21,20 @@ def main():
 
         mostrar_menu()
 
-        opcion = int(input("Ingrese el número de la opción deseada: "))
+        opcion = int(input("\n Ingrese el número de la opción deseada: "))
   
-        if opcion == '1':
-            opcion_a_suma()
-        elif opcion == '2':
-            opcion_b_producto()
-        elif opcion == '0':
-            print("Saliendo del programa. ¡Hasta luego!")
+        if opcion == 1:
+            
+              print(f'\n la suma de los numeros es: {opcion_a_suma(4,6)}')
+        
+        elif opcion == 2:
+            print(f'\n El producto de los numeros es: {opcion_b_producto(3,5)}')
+            
+        elif opcion == 0:
+            print("\n Saliendo del programa. ¡Hasta luego!")
             break
-       
-
-
-
-
+        else : 
+             print('\n Elija una opcion valida.')
 
 if __name__ == "__main__":
     main()
